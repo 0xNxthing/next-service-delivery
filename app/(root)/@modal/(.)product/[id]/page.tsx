07b8f6c6d51ef3e39f528dde9863f014a393ac87
Mikhail Revyakin <corergod@gmail.co>
2025-03-29 21:55:09 +0300

    init

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { ChooseProductModal } from '@/components/shared/modals/choose-product-modal';
 import prisma from '@/prisma/prisma-client';
 import { notFound } from 'next/navigation';
-import { PageProps } from 'next';
 
-// Типизация с использованием PageProps
-export default async function ProductModalPage({ params }: PageProps<{ id: string }>) {
+interface PageProps {
+	params: { id: string };
+}
+
+export default async function ProductModalPage({ params }: PageProps) {
 	const product = await prisma.product.findFirst({
 		where: {
 			id: Number(params.id),
